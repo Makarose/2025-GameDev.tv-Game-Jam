cb0e01b765a7e96ecf076921edb270a8600ecd13
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-18 16:03:21 -0700

    Create Ring Boundary as Area2D and implement bounce using raycasts on Player

--- player.gd
+++ player.gd
@@ -10,9 +10,16 @@ extends CharacterBody2D
 @export var up_gravity := 500
 @export var down_gravity := 600
 @export var jump_amount := 200
+@export var bounce_amount := 400
 
 var viewport_size: Vector2
 var coyote_time: float = 0.0
+var previous_player_position: Vector2
+
+@onready var left_ray_cast: RayCast2D = $LeftRayCast
+@onready var right_ray_cast: RayCast2D = $RightRayCast
+@onready var up_ray_cast: RayCast2D = $UpRayCast
+@onready var down_ray_cast: RayCast2D = $DownRayCast
 
 
 func _ready() -> void:
@@ -25,6 +32,17 @@ func _process(delta: float) -> void:
 		global_position.x = viewport_size.x + 16
 	elif global_position.x >= viewport_size.x + 16:
 		global_position.x = -16
+	
+	# ring boundary
+	if not left_ray_cast.is_colliding():
+		global_position = previous_player_position
+		velocity.x = bounce_amount
+	elif not right_ray_cast.is_colliding():
+		global_position = previous_player_position
+		velocity.x = -bounce_amount
+	elif not up_ray_cast.is_colliding():
+		global_position = previous_player_position
+		velocity.y = bounce_amount
 
 
 func _physics_process(delta: float) -> void:
@@ -45,7 +63,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		apply_friction(delta)
 	
+	# coyote jump
 	var was_on_floor := is_on_floor()
+	
+	previous_player_position = global_position
 	
 	move_and_slide()
 	
