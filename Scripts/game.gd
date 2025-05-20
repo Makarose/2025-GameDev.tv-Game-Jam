a084964bf8d6645285ce2eb304e99edaf4ef1061
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-20 14:21:36 -0700

    Implement procedural level generation: add LevelGenerator and Platform scenes

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 
 @onready var camera_2d: Camera = $Camera2D
+@onready var level_generator: Node2D = $LevelGenerator
 @onready var player: Player = $Player
 
 
 func _ready() -> void:
 	camera_2d.setup_camera(player)
+	level_generator.setup(player)
 
 
 func _process(delta: float) -> void:
