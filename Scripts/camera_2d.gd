d04719ffad4e82a2b360e95935fc91ea1c84fb1e
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-22 16:34:36 -0700

    Add KillZone below bottom of viewport to destroy platforms that pass offscreen

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -4,6 +4,10 @@ extends Camera2D
 var player: Player = null
 var viewport_size: Vector2
 
+@onready var kill_zone: Area2D = $KillZone
+@onready var kill_zone_shape: CollisionShape2D = $KillZone/CollisionShape2D
+
+
 func _ready() -> void:
 	viewport_size = get_viewport_rect().size
 	
@@ -12,6 +16,10 @@ func _ready() -> void:
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	# Initialize KillZone position and collision shape size
+	kill_zone.position.y = global_position.y + viewport_size.y
+	kill_zone_shape.shape.size = Vector2(viewport_size.x, 200)
 
 
 func _process(delta: float) -> void:
@@ -29,3 +37,8 @@ func _physics_process(delta: float) -> void:
 func setup_camera(_player: Player) -> void:
 	if _player:
 		player = _player
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	if body is Platform:
+		body.queue_free()
