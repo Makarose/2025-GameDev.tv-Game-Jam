5253943830d1ea3cb50dec0efbaaa5c1b8261237
Makarose <rosemarie.kaes@gmail.com>
2025-05-23 03:51:08 -0400

    Made parallax background, looped main song and added audioplayer to main game as child of camera.

---
+++ Scripts/background.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+@export var scrolling_speed = -40
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scroll_offset.y -= scrolling_speed*delta
