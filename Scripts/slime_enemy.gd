197a2f17b1023275b74b5afa34a9672f3c16f941
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-22 17:45:46 -0700

    Create SlimeEnemy

---
+++ Scripts/slime_enemy.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+@export var health: int = 3
+@export var damage: int = 1
+@export var move_speed: int = 40
+
+@onready var left_ray_cast: RayCast2D = $LeftRayCast2D
+@onready var right_ray_cast: RayCast2D = $RightRayCast2D
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if not left_ray_cast.is_colliding() or not right_ray_cast.is_colliding():
+		move_speed = -move_speed
+	position.x += move_speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("Slime Enemy Hit Player")
