c14dcac08e27a2af71e06648e76de88847756881
Makarose <rosemarie.kaes@gmail.com>
2025-05-25 04:34:11 -0400

    Fixed pause screen blurry font. Fixed scale on foreground. Added start platform to game node. Decorated ring. Added collision shapes to Dan. Tweaked a few things in my UI pages.

--- Scripts/end_page.gd
+++ Scripts/end_page.gd
@@ -16,3 +16,8 @@ func _on_texture_button_replay_pressed() -> void:
 
 func _on_texture_button_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_texture_button_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/start_page.tscn")
+	pass # Replace with function body.
