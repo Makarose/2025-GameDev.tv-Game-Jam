5e83c5e8ec54af2cc648efe6b778f07622b894ab
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-26 09:03:18 -0700

    Change all AudioStreamPlayer2Ds to AudioStreamPlayers in sfx_manager; set volume of all nodes to -5dB on ready

--- Scripts/sfx_manager.gd
+++ Scripts/sfx_manager.gd
@@ -1,20 +1,18 @@
 extends Node
 
 
-#func play_sfx(name: String) -> void:
-	#if name == "jump":
-		#audio_stream_player.stream = jump
-		#audio_stream_player.play()
-	#else:
-		#print("SFX name not found")
+@export var sfx_volume: float = -5.0
+
+
+func _ready() -> void:
+	for child in get_children():
+		if child is AudioStreamPlayer:
+			child.volume_db = sfx_volume
+
 
 # Plays a sound effect by the node name (must match one of the children)
 func play_sfx(name: String):
 	var sfx_node = get_node_or_null(name)
-	#print("Children of SfxManager:")
-	
-	#for child in get_children():
-		#print(child.name)
 	if sfx_node and sfx_node is AudioStreamPlayer:
 		sfx_node.play()
 	else:
